16ca9a1290881929ab60c56ed828aab2bf15b20d
ksingh-scogo <karan@scogo.in>
2025-03-17 02:20:55 +0530

    fixes to external DB installation

--- install_ext_pgdb.sh
+++ install_ext_pgdb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_VERSION="87"
-SCRIPT_URL="https://raw.githubusercontent.com/amidaware/tacticalrmm/master/install.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/ksingh-scogo/tacticalrmm/refs/heads/ksingh-v1.0.0/install_ext_pgdb.sh"
 
 sudo apt install -y curl wget dirmngr gnupg lsb-release ca-certificates
 sudo apt install -y software-properties-common
@@ -395,11 +395,9 @@ sudo mkdir /rmm
 sudo chown ${USER}:${USER} /rmm
 sudo mkdir -p /var/log/celery
 sudo chown ${USER}:${USER} /var/log/celery
-git clone https://github.com/amidaware/tacticalrmm.git /rmm/
+git clone https://github.com/ksingh-scogo/tacticalrmm.git /rmm/
 cd /rmm
-git config user.email "admin@example.com"
-git config user.name "Bob"
-git checkout master
+git checkout ksingh-v1.0.0
 
 sudo mkdir -p ${SCRIPTS_DIR}
 sudo chown ${USER}:${USER} ${SCRIPTS_DIR}
